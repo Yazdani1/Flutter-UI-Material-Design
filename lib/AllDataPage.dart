6ea810e4cc33985717c77ed14ec30de3ca5265e9
Yazdani <yaz4dani@gmail.com>
2019-04-01 21:00:51 +0600

    Second Commit

---
+++ lib/AllDataPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AllDataPage extends StatefulWidget {
+  @override
+  _AllDataPageState createState() => new _AllDataPageState();
+}
+
+class _AllDataPageState extends State<AllDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
+
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,7 +5,16 @@ class Home extends StatefulWidget {
   _HomeState createState() => new _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home>with SingleTickerProviderStateMixin {
+
+  TabController tabController;
+
+  @override
+  void initState() {
+    tabController=new TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -31,6 +40,71 @@ class _HomeState extends State<Home> {
                   elevation: 0.0,
                   child: new Icon(Icons.shopping_cart,color: Colors.black,size: 27.0,),
                 )
+              ],
+            ),
+          ),//container end
+          new Padding(
+              padding: const EdgeInsets.all(14.0),
+            child: new Text("Material UI",
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w500
+            ),
+            ),
+          ),
+          new Padding(
+              padding: EdgeInsets.all(15.0),
+            child: TabBar(
+              controller: tabController,
+              indicatorColor: Colors.red,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.9),
+              isScrollable: true,
+              tabs: <Widget>[
+
+                new Tab(
+                  child: new Text("First",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                  ),
+                  ),
+                ),
+                new Tab(
+                  child: new Text("Second",
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ),
+                new Tab(
+                  child: new Text("Third",
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ),
+
+                new Tab(
+                  child: new Text("Fourth",
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ),
+
+
+
+              ],
+            ),
+          ),//end tabbar
+
+          new Container(
+            height: MediaQuery.of(context).size.height-200.0,
+            child: TabBarView(
+                controller: tabController,
+              children: <Widget>[
+
               ],
             ),
           )
